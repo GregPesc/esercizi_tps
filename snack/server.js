f9c94d93b2b0017c60c8817f249467ded5b18970
GregPesc <gregorio.pescucci@polomanettiporciatti.edu.it>
2025-01-10 10:13:30 +0100

    Aggiunta route /cal/:calorie. Validazione input.

--- snack/server.js
+++ snack/server.js
@@ -14,7 +14,7 @@ app.put("/snack", (req, res) => {
         }
     });
 
-    let stmt = db.prepare(
+    const stmt = db.prepare(
         "INSERT INTO snack (nome, categoria, peso, prezzo, calorie) VALUES (?, ?, ?, ?, ?);"
     );
 
@@ -30,7 +30,17 @@ app.put("/snack", (req, res) => {
         });
     });
 
-    const snack = req.body;
+    const snack = req.query;
+
+    if (isNaN(parseFloat(snack.prezzo))) {
+        return res.status(400).send("Valore prezzo non valido");
+    }
+    if (isNaN(parseInt(snack.peso))) {
+        return res.status(400).send("Valore peso non valido");
+    }
+    if (isNaN(parseInt(snack.calorie))) {
+        return res.status(400).send("Valore calorie non valido");
+    }
 
     stmt.run(
         snack.nome.toLowerCase(),
@@ -66,7 +76,7 @@ app.get("/snack/:nome", (req, res) => {
         }
     });
 
-    let stmt = db.prepare("SELECT * FROM snack WHERE nome = ?;");
+    const stmt = db.prepare("SELECT * FROM snack WHERE nome = ?;");
 
     stmt.on("error", (error) => {
         console.error("Errore Statement:", error.message);
@@ -88,7 +98,6 @@ app.get("/snack/:nome", (req, res) => {
             res.json(rows);
         }
 
-        // Chiudiamo statement e database dopo aver inviato la risposta
         stmt.finalize((errFinalize) => {
             if (errFinalize)
                 console.error("Errore Finalize:", errFinalize.message);
@@ -108,7 +117,7 @@ app.get("/cat/:categoria", (req, res) => {
         }
     });
 
-    let stmt = db.prepare("SELECT * FROM snack WHERE categoria = ?;");
+    const stmt = db.prepare("SELECT * FROM snack WHERE categoria = ?;");
 
     stmt.on("error", (error) => {
         console.error("Errore Statement:", error.message);
@@ -130,7 +139,6 @@ app.get("/cat/:categoria", (req, res) => {
             res.json(rows);
         }
 
-        // Chiudiamo statement e database dopo aver inviato la risposta
         stmt.finalize((errFinalize) => {
             if (errFinalize)
                 console.error("Errore Finalize:", errFinalize.message);
@@ -141,14 +149,63 @@ app.get("/cat/:categoria", (req, res) => {
     });
 });
 
-// Crea db e avvia il server in ascolto
+// GET /cal/<quantità_calorie>  Recupera tutti gli snack che complessivamente forniscono meno di <quantità_calorie></quantità_calorie>
+app.get("/cal/:calorie", (req, res) => {
+    const limiteCalorie = parseInt(req.params.calorie);
 
+    if (isNaN(limiteCalorie)) {
+        return res.status(400).send("Valore calorie non valido");
+    }
+
+    const db = new sqlite3.Database("snack.db", (err) => {
+        if (err) {
+            console.error("Errore apertura DB:", err.message);
+            return res.status(500).send(err.message);
+        }
+    });
+
+    const stmt = db.prepare(
+        "SELECT * FROM snack WHERE peso * calorie / 100 < ?;"
+    );
+
+    stmt.on("error", (error) => {
+        console.error("Errore Statement:", error.message);
+        res.status(500).send(error.message);
+        stmt.finalize((errFinalize) => {
+            if (errFinalize)
+                console.error("Errore Finalize:", errFinalize.message);
+        });
+        db.close((errClose) => {
+            if (errClose) console.error("Errore Close:", errClose.message);
+        });
+    });
+
+    stmt.all(limiteCalorie, (err, rows) => {
+        if (err) {
+            console.error("Errore Query:", err.message);
+            res.status(500).send(err.message);
+        } else {
+            res.json(rows);
+        }
+
+        stmt.finalize((errFinalize) => {
+            if (errFinalize)
+                console.error("Errore Finalize:", errFinalize.message);
+        });
+        db.close((errClose) => {
+            if (errClose) console.error("Errore Close:", errClose.message);
+        });
+    });
+});
+
+// Crea file db
 const db = new sqlite3.Database("snack.db", (err) => {
     if (err) {
-        console.error("Errore apertura DB", err.message);
+        console.error("Errore apertura DB:", err.message);
     }
 });
 
+// Crea tabella Snack se non esiste
 db.run(
     `
     CREATE TABLE IF NOT EXISTS "Snack" (
@@ -162,18 +219,21 @@ db.run(
     `
 );
 
-// db.run(
-//     `
-//     INSERT INTO "Snack" ("nome", "categoria", "prezzo", "peso", "calorie")
-//     VALUES
-//     ('patatine classiche', 'salato', 1.99, 100, 500),
-//     ('patatine paprika', 'salato', 2.49, 80, 420),
-//     ('cioccolato al latte', 'dolce', 1.50, 50, 250),
-//     ('barretta energetica', 'salute', 1.99, 45, 180),
-//     ('biscotti integrali', 'dolce', 2.39, 120, 480);
-//     `
-// );
+// Aggiungi alcuni dati
+db.run(
+    `
+    INSERT INTO "Snack" ("nome", "categoria", "prezzo", "peso", "calorie")
+    VALUES
+    ('patatine classiche', 'salato', 1.99, 100, 500),
+    ('patatine paprika', 'salato', 2.49, 80, 420),
+    ('cioccolato al latte', 'dolce', 1.50, 50, 250),
+    ('barretta energetica', 'salute', 1.99, 45, 180),
+    ('biscotti integrali', 'dolce', 2.39, 120, 480);
+    `,
+    (err) => {}
+);
 
-app.listen(10000, () => {
-    console.log("Server in ascolto sulla porta 10000");
+// Avvia il server in ascolto
+app.listen(20000, () => {
+    console.log("Server in ascolto sulla porta 20000");
 });
