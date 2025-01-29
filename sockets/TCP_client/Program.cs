5f06b35a4566cc9aac058ee508b2e37e2a768aad
GregPesc <gregorio.pescucci@polomanettiporciatti.edu.it>
2025-01-29 13:14:02 +0100

    socket TCP. Client e Server.

---
+++ sockets/TCP_client/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+class TcpClientExample
+{
+    public static void Start()
+    {
+        using (TcpClient client = new TcpClient("127.0.0.1", 8888))
+        using (NetworkStream stream = client.GetStream())
+        {
+            Console.WriteLine("Messaggio da inviare: ");
+            string message = Console.ReadLine() ?? "Hello, world!";
+            byte[] data = Encoding.UTF8.GetBytes(message);
+
+            stream.Write(data, 0, data.Length);
+            Console.WriteLine($"Inviato: {message}");
+
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine($"Ricevuto: {response}");
+        }
+    }
+}
+
+
+
+class Program
+{
+    public static void Main()
+    {
+        TcpClientExample.Start();
+    }
+}
---
+++ sockets/TCP_server/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class TcpServer
+{
+
+    public static void Start()
+    {
+        const int port = 8888;
+        TcpListener server = new TcpListener(IPAddress.Any, port);
+        server.Start();
+        Console.WriteLine($"Server in ascolto su porta {port}...");
+
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient();
+            Console.WriteLine("Client connesso.");
+            Thread clientThread = new Thread(HandleClient);
+            clientThread.Start(client);
+        }
+    }
+
+    private static void HandleClient(object obj)
+    {
+
+        using (TcpClient client = (TcpClient)obj)
+        using (NetworkStream stream = client.GetStream())
+        {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                string data = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Ricevuto: {data}");
+
+                // Echo del messaggio
+                byte[] response = Encoding.UTF8.GetBytes(data);
+                stream.Write(response, 0, response.Length);
+            }
+        }
+    }
+}
+
+
+
+class Program
+{
+    public static void Main()
+    {
+        TcpServer.Start();
+    }
+}
